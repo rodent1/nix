501d7bab8a248489ae14d2f745836b21095f1b25
Stian R. Sporaland <mail@stianrs.dev>
2024-03-03 15:28:48 +0000

    Update atuin configuration

--- home-manager/common/shell/atuin/default.nix
+++ home-manager/common/shell/atuin/default.nix
@@ -1,7 +1,22 @@
-{ }:
 {
-  sync_address = "https://sh.rodent.cc";
-  auto_sync = true;
-  sync_frequency = "1m";
-  search_mode = "fuzzy";
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  programs.atuin = {
+    enable = true;
+    package = pkgs.unstable.atuin;
+
+    flags = [
+      "--disable-up-arrow"
+    ];
+
+    settings = {
+      sync_address = "https://sh.rodent.cc";
+      auto_sync = true;
+      sync_frequency = "1m";
+      search_mode = "fuzzy";
+    };
+  };
 }
