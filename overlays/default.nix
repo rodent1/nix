f2b10c74fcdacb67bf4f5acba3b06cd87a7c698b
Stian Rossavik Sporaland <mail@stianrs.dev>
2024-03-03 19:42:59 +0100

    Add kubecolor package

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,6 +10,18 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+    # kubecolor = prev.kubecolor.overrideAttrs (oldAttrs: {
+    #   version = "v0.0.25";
+    #   src = prev.fetchFromGitHub {
+    #     owner = "hidetatz";
+    #     repo = "kubecolor";
+    #     rev = "2ce3b8b099a44b6f0d0d0b62ae228a65c437e6a5";
+    #     # obtained from `nix-shell -p nix-prefetch-github --run "nix-prefetch-github hidetatz kubecolor --rev v0.0.25"`
+    #     hash = "sha256-FyKTI7Br9BjSpmf9ch2E4EZAWM7/jowZfRrCn4GTcps=";
+    #   };
+    #   buildInputs = oldAttrs.buildInputs or [] ++ [final.unstable.go];
+    # });
+
   };
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
