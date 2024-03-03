6ef4e2b1f57282c9fb7e417acfcca6337e876b1c
Stian Rossavik Sporaland <mail@stianrs.dev>
2024-03-03 12:43:33 +0000

    Enable git and configure git settings

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -57,8 +57,56 @@
 
   # Enable home-manager and git
   programs.home-manager.enable = true;
-  programs.git.enable = true;
+
   programs.gh.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Stian Rossavik Sporaland";
+    userEmail = "mail@stianrs.dev";
+
+    extraConfig = {
+      core = {
+        autocrlf = "input";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      pull = {
+        rebase = true;
+      };
+      rebase = {
+        autoStash = true;
+      };
+    };
+    aliases = {
+      co = "checkout";
+      pl = "pull";
+      l = "log --pretty=format:\"%C(yellow)%h%Cred%d\\ %Creset%s%Cblue\\ (%cn)\" --decorate";
+    };
+    ignores = [
+      # Mac OS X hidden files
+      ".DS_Store"
+      # Windows files
+      "Thumbs.db"
+      # asdf
+      ".tool-versions"
+      # mise
+      ".mise.toml"
+      # Sops
+      ".decrypted~*"
+      "*.decrypted.*"
+      # Python virtualenvs
+      ".venv"
+    ];
+  };
+  programs.gpg.enable = true;
+
+  home.packages = [
+    pkgs.git-filter-repo
+    pkgs.pinentry
+    pkgs.tig
+  ];
+
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
