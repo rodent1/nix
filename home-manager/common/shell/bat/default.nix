7b5f70e5ec5e1f4cf191a73ad8338fb6ece2b707
Stian R. Sporaland <mail@stianrs.dev>
2024-03-03 14:41:45 +0000

    Add bat shell configuration

---
+++ home-manager/common/shell/bat/default.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  programs = {
+    bat = {
+      enable = true;
+      config = import  ./defaultConfig.nix;
+      themes = {
+        catppuccin-macchiato = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+            sha256 = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          };
+          file = "Catppuccin-macchiato.tmTheme";
+        };
+      };
+    };
+
+    fish = {
+      shellAliases = {
+        cat = "bat";
+      };
+    };
+  };
+}
