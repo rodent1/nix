c64945b8cf07c1197b92152590ccac9ca0411ec8
Stian R. Sporaland <mail@stianrs.dev>
2024-03-03 10:24:24 +0000

    Enable gh program in home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -58,6 +58,7 @@
   # Enable home-manager and git
   programs.home-manager.enable = true;
   programs.git.enable = true;
+  programs.gh.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
