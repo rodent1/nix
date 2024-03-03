39698b10ff5949b589b0371102daaa389d9ee9c7
Stian Rossavik Sporaland <stian.sporaland@pm.me>
2024-03-03 10:07:53 +0100

    Update nix-ld-rs and remove nix-ld-vscode

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,7 +16,6 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    inputs.nix-ld-vscode.nixosModules.default
   ];
 
   nixpkgs = {
--- hosts/common/optional/wsl.nix
+++ hosts/common/optional/wsl.nix
@@ -1,5 +1,10 @@
 { inputs, pkgs, ... }:
 {
+
+  nixpkgs.overlays = [
+    inputs.nix-ld-rs.overlays.default
+  ];
+
   imports = [
     inputs.nixos-wsl.nixosModules.default
     inputs.vscode-server.nixosModules.default
@@ -10,13 +15,11 @@
     defaultUser = "stianrs";
     startMenuLaunchers = true;
     wslConf.automount.root = "/mnt";
-    extraBin = with pkgs; [
-      { src = "${coreutils}/bin/uname"; }
-      { src = "${coreutils}/bin/dirname"; }
-      { src = "${coreutils}/bin/readlink"; }
-    ];
   };
 
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    package = pkgs.nix-ld-rs;
+  };
   services.vscode-server.enable = true;
 }
