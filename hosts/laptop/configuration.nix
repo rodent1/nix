70e2a70e7407e982a67daf4a932e1ac90c2fff36
Stian Rossavik Sporaland <stian.sporaland@pm.me>
2024-03-03 09:43:54 +0100

    Update home-manager and laptop configuration files

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,5 +1,3 @@
-# This is your system's configuration file.
-# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 {
   inputs,
   outputs,
@@ -20,73 +18,15 @@
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
 
+    inputs.home-manager.nixosModules.home-manager
+    ../../common
+    ./../common/optional/wsl.nix
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
-
-    # Import home-manager's NixOS module
-    inputs.home-manager.nixosModules.home-manager
-
-    # Import WSL's NixOS module
-    inputs.nixos-wsl.nixosModules.wsl
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
-      outputs.overlays.additions
-      outputs.overlays.modifications
-      outputs.overlays.unstable-packages
 
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-    };
-  };
-
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
-  environment.etc =
-    lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
-
-  nix.settings = {
-    # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
-    # Deduplicate and optimize nix store
-    auto-optimise-store = true;
-  };
-
-  # TODO: Move to dedicated WSL settings
-
-  wsl = {
-    enable = true;
-    defaultUser = "stianrs";
-    startMenuLaunchers = true;
-    wslConf.automount.root = "/mnt";
-  };
-
-  networking.hostName = "gamer";
+  networking.hostName = "laptop";
 
   # TODO: Move to dedicated user settings
 
