260a13d03590ebf6d737a1c8c1477b80b3773302
Stian R. Sporaland <mail@stianrs.dev>
2024-03-28 09:01:14 +0100

    Add support for stern completion in fish shell

--- homes/_modules/kubernetes/stern/default.nix
+++ homes/_modules/kubernetes/stern/default.nix
@@ -1,6 +1,14 @@
 { pkgs, lib, config, ... }:
 let cfg = config.modules.kubernetes;
 in {
-  config = lib.mkMerge
-    [ (lib.mkIf cfg.enable { home.packages = [ pkgs.unstable.stern ]; }) ];
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      home.packages = [ pkgs.unstable.stern ];
+      programs.fish = {
+        interactiveShellInit = ''
+          ${pkgs.unstable.stern}/bin/stern --completion fish | source
+        '';
+      };
+    })
+  ];
 }
