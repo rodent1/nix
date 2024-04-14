66f53631e5cb0b424c27b7d5c401ced6874d91af
Stian R. Sporaland <mail@stianrs.dev>
2024-04-14 17:41:03 +0200

    Update eza shell aliases in default.nix

--- homes/_modules/shell/eza/default.nix
+++ homes/_modules/shell/eza/default.nix
@@ -3,7 +3,18 @@
     programs.eza = {
       enable = true;
       icons = true;
-      enableAliases = true;
+      enableAliases = false;
+    };
+
+    programs.fish = {
+      shellAliases = {
+        ls = "eza";
+        la = "eza -al --group-directories-first";
+        ld = "eza -lD";
+        ll = "eza -l --group-directories-first";
+        lh = "eza -dl .* --group-directories-first";
+        lt = "eza -al --sort=modified";
+      };
     };
   };
 }
