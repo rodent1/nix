a7f0b45f43a62580d8f92dfc8ad3570c3da7b847
Stian R. Sporaland <mail@stianrs.dev>
2024-04-14 17:27:03 +0200

    Fix tmux autostart

--- homes/_modules/shell/tmux/default.nix
+++ homes/_modules/shell/tmux/default.nix
@@ -87,6 +87,16 @@ in {
           }
         ];
       };
+      programs.fish.interactiveShellInit = ''
+        # only run in interactive (not automated SSH for example)
+        if status is-interactive
+        # don't nest inside another tmux
+        and not set -q TMUX
+          # Adapted from https://unix.stackexchange.com/a/176885/347104
+          # Create session 'main' or attach to 'main' if already exists.
+          tmux new-session -A -s main
+        end
+      '';
     })
   ];
 }
