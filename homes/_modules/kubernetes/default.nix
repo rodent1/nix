b162b6938be6773c4cc7f7cf77288fa81313e6db
Stian R. Sporaland <mail@stianrs.dev>
2024-05-04 18:20:26 +0200

    Add helm

--- homes/_modules/kubernetes/default.nix
+++ homes/_modules/kubernetes/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./flux
     ./helmfile
+    ./helm
     ./k9s
     ./krew
     ./kubecm
---
+++ homes/_modules/kubernetes/helm/default.nix
@@ -0,0 +1,12 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.modules.kubernetes;
+in {
+  config =
+    lib.mkMerge
+    [(lib.mkIf cfg.enable {home.packages = [pkgs.kubernetes-helm];})];
+}
