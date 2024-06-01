3f8df980d1d8962294db2cd61c859c883889e35a
Stian R. Sporaland <mail@stianrs.dev>
2024-06-01 13:52:30 +0200

    chore: Update atuin configuration for key path

--- homes/_modules/shell/atuin/default.nix
+++ homes/_modules/shell/atuin/default.nix
@@ -1,13 +1,12 @@
-{pkgs, ...}: {
+{config, ...}: {
   config = {
     programs.atuin = {
       enable = true;
-      package = pkgs.unstable.atuin;
-
       flags = ["--disable-up-arrow"];
 
       settings = {
         sync_address = "https://sh.rodent.cc";
+        key_path = "${config.xdg.configHome}/atuin/key";
         auto_sync = true;
         sync_frequency = "1m";
         search_mode = "fuzzy";
