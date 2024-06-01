161ae6679e4b7cf004f41ec89f9a2db035fa82c8
Stian R. Sporaland <mail@stianrs.dev>
2024-06-01 11:12:13 +0200

    chore: Update kubecolor to version 0.0.25

--- pkgs/kubecolor.nix
+++ pkgs/kubecolor.nix
@@ -5,24 +5,28 @@
 }:
 buildGoModule rec {
   pname = "kubecolor";
-  version = "0.0.20";
+  version = "0.0.25";
 
   src = fetchFromGitHub {
     owner = "hidetatz";
-    repo = pname;
+    repo = "kubecolor";
     rev = "v${version}";
-    sha256 = "sha256-bKHEp9AxH1CcObhNzD3BkNOdyWZu7JrEdsXpo49wEcI=";
+    hash = "sha256-FyKTI7Br9BjSpmf9ch2E4EZAWM7/jowZfRrCn4GTcps=";
   };
 
-  vendorHash = "sha256-C1K7iEugA4HBLthcOI7EZ6H4YHW6el8X6FjVN1BeJR0=";
+  vendorHash = "sha256-DLj7ztOFNmDru1sO+ezecQeRbIbOq49M4EcJuWLNstI=";
 
-  ldflags = ["-s" "-w" "-X main.Version=${version}"];
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.Version=${version}"
+  ];
 
   meta = with lib; {
     description = "Colorizes kubectl output";
     homepage = "https://github.com/hidetatz/kubecolor";
-    changelog = "https://github.com/hidetatz/kubecolor/releases/tag/v${version}";
     license = licenses.mit;
-    maintainers = with maintainers; [ivankovnatsky SuperSandro2000];
+    maintainers = with maintainers; [];
+    mainProgram = "kubecolor";
   };
 }
