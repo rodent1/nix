f9497e19079433245817a42a57a40727e04f889f
Stian R. Sporaland <mail@stianrs.dev>
2024-07-07 10:29:41 +0200

    chore: move atuin to unstable and add records sync

--- homes/_modules/shell/atuin/default.nix
+++ homes/_modules/shell/atuin/default.nix
@@ -1,14 +1,16 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   config = {
     programs.atuin = {
       enable = true;
+      package = pkgs.unstable.atuin;
       flags = [ "--disable-up-arrow" ];
 
       settings = {
         sync_address = "https://sh.rodent.cc";
         key_path = "${config.xdg.configHome}/atuin/key";
         auto_sync = true;
+        sync.records = true;
         sync_frequency = "1m";
         search_mode = "fuzzy";
       };
