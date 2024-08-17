8503cf3a19e97165e0c45560689367bc15a70631
Stian R. Sporaland <mail@stianrs.dev>
2024-08-17 13:15:47 +0200

    chore: Clean up zellij config

--- homes/_modules/shell/zellij/default.nix
+++ homes/_modules/shell/zellij/default.nix
@@ -1,45 +1,28 @@
 { config, lib, ... }:
 let
   cfg = config.modules.shell.zellij;
+  cmd = lib.getExe config.programs.zellij.package;
 in
 {
   options.modules.shell.zellij = {
     enable = lib.mkEnableOption "zellij";
   };
 
-  config = lib.mkMerge [
-    (lib.mkIf cfg.enable {
-      programs.zellij = {
-        enable = true;
-        settings = {
-          theme = "catppuccin-macchiato";
-        };
+  config = lib.mkIf (cfg.enable) {
+    programs.zellij = {
+      enable = true;
+      settings = {
+        theme = "catppuccin-macchiato";
+        copy_command = "shcopy";
       };
+    };
 
-      programs.fish.interactiveShellInit = ''
-        if not set -q TERM_PROGRAM; or not string match -q "vscode" $TERM_PROGRAM
-          if not set -q ZELLIJ
-              if test "$ZELLIJ_AUTO_ATTACH" = "true"
-                  if not set -q ZELLIJ_AUTO_ATTACH_SESSION_NAME
-                      zellij attach -c
-                  else
-                      zellij attach -c $ZELLIJ_AUTO_ATTACH_SESSION_NAME
-                  end
-              else
-                  zellij
-              end
-
-              if test "$ZELLIJ_AUTO_EXIT" = "true"
-                  kill $fish_pid
-              end
-          end
-        end
-      '';
-
-      home.sessionVariables = {
-        ZELLIJ_AUTO_ATTACH = "true";
-        ZELLIJ_AUTO_ATTACH_SESSION_NAME = "main";
-      };
-    })
-  ];
+    programs.fish.interactiveShellInit = ''
+      if not string match -q "vscode" $TERM_PROGRAM
+        set -gx ZELLIJ_AUTO_ATTACH true
+        set -gx ZELLIJ_AUTO_EXIT false
+        eval (${cmd} setup --generate-auto-start fish | string collect)
+      end
+    '';
+  };
 }
