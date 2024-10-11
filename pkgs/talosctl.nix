bbdc64588d500db6f25d5f58e736cbf07bd32888
Stian R. Sporaland <mail@stianrs.dev>
2024-10-11 19:34:42 +0200

    fix talosctl vendorHash

--- pkgs/talosctl.nix
+++ pkgs/talosctl.nix
@@ -3,43 +3,42 @@
   lib,
   installShellFiles,
   ...
-}:
-let
-  sourceData = pkgs.callPackage ./_sources/generated.nix { };
+}: let
+  sourceData = pkgs.callPackage ./_sources/generated.nix {};
   packageData = sourceData.talosctl;
 in
-pkgs.unstable.buildGo123Module {
-  inherit (packageData) pname src;
-  version = lib.strings.removePrefix "v" packageData.version;
-  vendorHash = "sha256-9qkealjjdBO659fdWdgFii3ThPRwKpYasB03L3Bktqs=";
+  pkgs.unstable.buildGo123Module {
+    inherit (packageData) pname src;
+    version = lib.strings.removePrefix "v" packageData.version;
+    vendorHash = "sha256-aTtvVpL979BUvSBwBqRqCWSWIBBmmty9vBD97Q5P4+E=";
 
-  ldflags = [
-    "-s"
-    "-w"
-  ];
+    ldflags = [
+      "-s"
+      "-w"
+    ];
 
-  # This is needed to deal with workspace issues during the build
-  overrideModAttrs = _: { GOWORK = "off"; };
-  GOWORK = "off";
+    # This is needed to deal with workspace issues during the build
+    overrideModAttrs = _: {GOWORK = "off";};
+    GOWORK = "off";
 
-  subPackages = [ "cmd/talosctl" ];
+    subPackages = ["cmd/talosctl"];
 
-  nativeBuildInputs = [ installShellFiles ];
+    nativeBuildInputs = [installShellFiles];
 
-  postInstall = ''
-    installShellCompletion --cmd talosctl \
-      --bash <($out/bin/talosctl completion bash) \
-      --fish <($out/bin/talosctl completion fish) \
-      --zsh <($out/bin/talosctl completion zsh)
-  '';
+    postInstall = ''
+      installShellCompletion --cmd talosctl \
+        --bash <($out/bin/talosctl completion bash) \
+        --fish <($out/bin/talosctl completion fish) \
+        --zsh <($out/bin/talosctl completion zsh)
+    '';
 
-  doCheck = false; # no tests
+    doCheck = false; # no tests
 
-  meta = with lib; {
-    description = "A CLI for out-of-band management of Kubernetes nodes created by Talos";
-    homepage = "https://www.talos.dev/";
-    license = licenses.mpl20;
-    mainProgram = "talosctl";
-    maintainers = with maintainers; [ flokli ];
-  };
-}
+    meta = with lib; {
+      description = "A CLI for out-of-band management of Kubernetes nodes created by Talos";
+      homepage = "https://www.talos.dev/";
+      license = licenses.mpl20;
+      mainProgram = "talosctl";
+      maintainers = with maintainers; [flokli];
+    };
+  }
