ef7910c47861c1c188fa51f8f3b9075cf12edd88
Stian R. Sporaland <mail@stianrs.dev>
2025-04-11 14:35:39 +0200

    atuin

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,9 @@
   ...
 }:
 {
+  atuin = pkgs.callPackage ./atuin.nix {
+    toolchain = pkgs.unstable.fenix.minimal.toolchain;
+  };
   gh-copilot = pkgs.callPackage ./gh-copilot.nix { };
   gh-tidy = pkgs.callPackage ./gh-tidy.nix { };
   kubecolor-catppuccin = pkgs.callPackage ./kubecolor-catppuccin.nix { };
