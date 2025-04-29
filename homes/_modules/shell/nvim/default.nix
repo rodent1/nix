0849458147e3f346649c38be1787739fa5b7e2ca
Stian R. Sporaland <mail@stianrs.dev>
2025-04-29 07:37:30 +0200

    fix: neovim unstable is broken, switch to stable or now

--- homes/_modules/shell/nvim/default.nix
+++ homes/_modules/shell/nvim/default.nix
@@ -16,7 +16,7 @@ in
 
     programs.neovim = {
       enable = true;
-      package = pkgs.unstable.neovim-unwrapped;
+      package = pkgs.neovim-unwrapped;
 
       viAlias = true;
       vimAlias = true;
