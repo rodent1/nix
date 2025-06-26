6ccb5e0c88f50d47c414c7aac1120939483ae6d5
Stian R. Sporaland <mail@stianrs.dev>
2025-06-26 10:33:20 +0200

    clean up web dev module

--- homes/_modules/development/web/default.nix
+++ homes/_modules/development/web/default.nix
@@ -8,18 +8,18 @@ let
   cfg = config.modules.development;
 
   # Override pnpm to use specific Node.js version
-  pnpm-with-custom-node = pkgs.unstable.pnpm.override {
-    nodejs = pkgs.unstable.nodejs_24;
+  pnpm-with-custom-node = pkgs.pnpm.override {
+    nodejs = pkgs.nodejs_24;
   };
 in
 {
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs.unstable; [
-      biome
-      bun
+    home.packages = with pkgs; [
       nodejs_24
       pnpm-with-custom-node
       turbo
+      unstable.biome
+      unstable.bun
     ];
   };
 }
