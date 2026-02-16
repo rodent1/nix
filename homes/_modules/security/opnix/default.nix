b60b1d3953a3fdb2814e8dc6c8a7938788034a01
stianrs <mail@stianrs.dev>
2026-02-16 19:58:33 +0100

    fix: update paths for onepassword-secrets configuration

--- homes/_modules/security/opnix/default.nix
+++ homes/_modules/security/opnix/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ lib, isWSL, ... }:
 {
   config = {
     programs.onepassword-secrets = {
@@ -7,29 +7,28 @@
       secrets = {
         ageKey = {
           reference = "op://dotfiles/age key/Key";
-          path = "${config.xdg.configHome}/age/keys.txt";
+          path = ".config/age/keys.txt";
           mode = "0600";
         };
 
         atuinKey = {
           reference = "op://dotfiles/Atuin/key64";
-          path = "${config.xdg.configHome}/atuin/key";
+          path = ".config/atuin/key";
           mode = "0600";
         };
 
         mcConfig = {
           reference = "op://dotfiles/mc-config/config.json";
-          path = "${config.home.homeDirectory}/.mc/config.json";
+          path = ".mc/config.json";
           mode = "0600";
         };
 
-        onepassToken = {
+        onepassToken = lib.mkIf isWSL {
           reference = "op://dotfiles/1password/OP_SERVICE_ACCOUNT_TOKEN";
-          path = "${config.xdg.configHome}/1Password/op-service-account-token";
+          path = ".config/1Password/op-service-account-token";
           mode = "0600";
         };
       };
     };
   };
 }
-  
