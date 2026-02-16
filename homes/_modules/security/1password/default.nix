0ca7e24f3c86877fdb902458055d7e6f9f5cb4ef
stianrs <mail@stianrs.dev>
2026-02-16 08:49:10 +0100

    update secret config

--- homes/_modules/security/1password/default.nix
+++ homes/_modules/security/1password/default.nix
@@ -5,17 +5,23 @@
   ...
 }:
 {
-  config = lib.mkIf (!isWSL) {
-    xdg.configFile."1Password/ssh/agent.toml" = {
-      text = ''
-        [[ssh-keys]]
-        item = "Personal"
-        vault = "dotfiles"
+  config =
+    lib.mkIf (!isWSL) {
+      xdg.configFile."1Password/ssh/agent.toml" = {
+        text = ''
+          [[ssh-keys]]
+          item = "Personal"
+          vault = "dotfiles"
+        '';
+      };
+
+      home.sessionVariables = {
+        SSH_AUTH_SOCK = "${config.home.homeDirectory}/.1password/agent.sock";
+      };
+    }
+    // lib.mkIf isWSL {
+      programs.fish.loginShellInit = ''
+        set -gx OP_SERVICE_ACCOUNT_TOKEN (cat ${config.xdg.configHome}/1password/op-service-account-token)
       '';
     };
-
-    home.sessionVariables = {
-      SSH_AUTH_SOCK = "${config.home.homeDirectory}/.1password/agent.sock";
-    };
-  };
 }
--- homes/_modules/security/opnix/default.nix
+++ homes/_modules/security/opnix/default.nix
@@ -5,9 +5,27 @@
       enable = true;
       tokenFile = "/etc/opnix-token";
       secrets = {
+        ageKey = {
+          reference = "op://dotfiles/age key/Key";
+          path = "${config.xdg.configHome}/age/keys.txt";
+          mode = "0600";
+        };
+
         atuinKey = {
           reference = "op://dotfiles/Atuin/key64";
-          path = "${config.home.homeDirectory}/.config/atuin/key";
+          path = "${config.xdg.configHome}/atuin/key";
+          mode = "0600";
+        };
+
+        mcConfig = {
+          reference = "op://dotfiles/mc-config/config.json";
+          path = "${config.home.homeDirectory}/.mc/config.json";
+          mode = "0600";
+        };
+
+        onepassToken = {
+          reference = "op://dotfiles/1password/OP_SERVICE_ACCOUNT_TOKEN";
+          path = "${config.xdg.configHome}/1password/op-service-account-token";
           mode = "0600";
         };
       };
