84cc2cb4f040fbe394775b3888f561be340dc89b
Stian R. Sporaland <mail@stian.rs>
2026-04-20 21:10:41 +0200

    hyprland config

--- homes/stianrs/hosts/gamer.nix
+++ homes/stianrs/hosts/gamer.nix
@@ -13,8 +13,11 @@ in
 
   config = lib.mkIf cfg.enable {
     wayland.windowManager.hyprland = {
-      enable = true;
-      systemd.enable = false; # Managed by UWSM in the NixOS module
+      extraConfig = ''
+        monitor = DP-1, 2560x1440@143.91Hz, 2560x-1800, 1, cm, srgb
+        monitor = DP-2, 2560x1440@165.07Hz, 2560x-350, 1, cm, srgb
+        monitor = DP-3, 1920x1080@143.98Hz, 5130x-580, 1, transform, 3, cm, srgb
+      '';
     };
 
     programs.waybar.enable = true;
