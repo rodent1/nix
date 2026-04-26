ce13ba4c3246a4d5c9dd99922077fb65f4c766ba
Stian R. Sporaland <mail@stian.rs>
2026-04-26 21:11:49 +0200

    prefer-no-csd

--- homes/_modules/desktop/niri/settings.nix
+++ homes/_modules/desktop/niri/settings.nix
@@ -30,6 +30,11 @@ in
       hotkey-overlay = {
         skip-at-startup = true;
       };
+
+      # Uncomment this line to ask the clients to omit their client-side decorations if possible.
+      # If the client will specifically ask for CSD, the request will be honored.
+      # Additionally, clients will be informed that they are tiled, removing some rounded corners.
+      prefer-no-csd = true;
     };
   };
 }
