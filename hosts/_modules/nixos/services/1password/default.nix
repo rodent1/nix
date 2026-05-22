3683bb7ab539da941358352d4552e7febe67a564
Stian R. Sporaland <mail@stian.rs>
2026-05-22 21:38:35 +0200

    update lockfile

--- hosts/_modules/nixos/services/1password/default.nix
+++ hosts/_modules/nixos/services/1password/default.nix
@@ -13,7 +13,7 @@ in
     programs._1password.enable = true;
     programs._1password-gui = lib.mkIf (!isWSL) {
       enable = true;
-      package = pkgs.unstable._1password-gui;
+      # package = pkgs.unstable._1password-gui;
       # Certain features, including CLI integration and system authentication support,
       # require enabling PolKit integration on some desktop environments (e.g. Plasma).
       polkitPolicyOwners = [ "stianrs" ];
