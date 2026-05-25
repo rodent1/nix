cc0e62519f2fff29b5fd60c3be636557fe7ac018
Stian R. Sporaland <mail@stian.rs>
2026-05-25 09:40:49 +0200

    feat(wagoapp): add nvfetcher automation

--- pkgs/wagoapp.nix
+++ pkgs/wagoapp.nix
@@ -1,19 +1,13 @@
 {
+  callPackage,
   appimageTools,
-  fetchurl,
 }:
-
 let
-  pname = "WagoApp";
-  version = "2.9.11";
-  src = fetchurl {
-    url = "https://wago-addons.ams3.digitaloceanspaces.com/wagoapp/WagoApp_${version}.AppImage";
-    hash = "sha256-vsRC5Q5nXo//AGf1rZnyiRalX1mQKqhFsbecXml8N30=";
-  };
-  appimageContents = appimageTools.extract { inherit pname version src; };
+  sourceData = callPackage _sources/generated.nix { };
 in
 appimageTools.wrapType2 rec {
-  inherit pname version src;
+  inherit (sourceData.wagoapp) pname version src;
+  appimageContents = appimageTools.extract { inherit pname version src; };
 
   extraInstallCommands = ''
     install -m 444 -D ${appimageContents}/WagoApp.desktop $out/share/applications/WagoApp.desktop
