6075d00cb6b1b44307441ce3883f830461709899
Stian R. Sporaland <mail@stian.rs>
2026-06-01 14:59:49 +0200

    fix silly treefmt import

--- modules/flake/treefmt.nix
+++ modules/flake/treefmt.nix
@@ -1,1 +1,23 @@
-{ inputs, ... }: import ../../lib/treefmt.nix { inherit inputs; }
+{ inputs, ... }:
+{
+  imports = [
+    inputs.treefmt-nix.flakeModule
+  ];
+
+  perSystem =
+    { ... }:
+    {
+      treefmt.config = {
+        projectRootFile = "flake.nix";
+        flakeCheck = true;
+        programs.nixfmt.enable = true;
+
+        settings = {
+          global.excludes = [
+            "**/hardware-configuration.nix"
+            "pkgs/_sources/*"
+          ];
+        };
+      };
+    };
+}
