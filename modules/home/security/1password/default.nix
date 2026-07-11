55eed397b6d0c610f4266c34ba6ab0921b20c4de
Stian R. Sporaland <mail@stian.rs>
2026-07-11 21:59:47 +0200

    fix(security): scope WSL 1Password token

--- modules/home/security/1password/default.nix
+++ modules/home/security/1password/default.nix
@@ -1,6 +1,11 @@
 {
   rodent.homeModules.default =
-    { config, lib, ... }:
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
     {
       config = {
         xdg.configFile."1Password/ssh/agent.toml" = lib.mkIf (!config.rodent.isWSL) {
@@ -15,9 +20,11 @@
           SSH_AUTH_SOCK = "${config.home.homeDirectory}/.1password/agent.sock";
         };
 
-        programs.fish.loginShellInit = lib.mkIf config.rodent.isWSL ''
-          set -gx OP_SERVICE_ACCOUNT_TOKEN (cat ${config.xdg.configHome}/1Password/op-service-account-token)
-        '';
+        programs.fish.functions.op = lib.mkIf config.rodent.isWSL {
+          body = ''
+            env OP_SERVICE_ACCOUNT_TOKEN="$(cat ${config.xdg.configHome}/1Password/op-service-account-token)" ${pkgs._1password-cli}/bin/op $argv
+          '';
+        };
       };
     };
 }
