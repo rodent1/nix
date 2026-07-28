c1dede35cbf91c52c3eb484dcea0eb97e6134dc6
Stian R. Sporaland <mail@stian.rs>
2026-07-28 10:12:38 +0200

    feat(firefox): enable unstable Firefox package and add language packs

--- modules/home/desktop/apps/firefox.nix
+++ modules/home/desktop/apps/firefox.nix
@@ -1,10 +1,21 @@
 {
   internal.homeModules.desktop =
-    { config, lib, ... }:
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
     {
       config = lib.mkIf config.modules.desktop.enable {
         programs.firefox = {
           enable = true;
+          package = pkgs.unstable.firefox;
+
+          languagePacks = [
+            "en-GB"
+            "no-NO"
+          ];
         };
       };
     };
