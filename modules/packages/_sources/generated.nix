8200e2750fbee024c1001ae801e6a536af165b17
rodent1 <24843521+rodent1@users.noreply.github.com>
2026-08-06 04:04:28 +0000

    chore(deps): Update packages managed by nvfetcher

--- modules/packages/_sources/generated.nix
+++ modules/packages/_sources/generated.nix
@@ -16,10 +16,10 @@
   };
   kubectl-kopiur = {
     pname = "kubectl-kopiur";
-    version = "0.9.2";
+    version = "0.9.3";
     src = fetchurl {
-      url = "https://github.com/home-operations/kopiur/releases/download/0.9.2/kubectl-kopiur_0.9.2_linux_amd64.tar.gz";
-      sha256 = "sha256-3k3E4sauv0BIpO5wltDAggQlGGYBdzWf1xZ16iLJhxk=";
+      url = "https://github.com/home-operations/kopiur/releases/download/0.9.3/kubectl-kopiur_0.9.3_linux_amd64.tar.gz";
+      sha256 = "sha256-THOjwpuDPG0O88JNS/YsncYQAEmNNYOhUFrGDEwX2b4=";
     };
   };
   wagoapp = {
