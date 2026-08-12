45207b3aadc31128a1f5c74dbd0ce54a043ec9fd
Stian R. Sporaland <mail@stian.rs>
2026-08-12 17:47:45 +0200

    Remove the ChatGPT desktop package

--- modules/packages/chatgpt-desktop.nix
+++
@@ -1,9 +0,0 @@
-{
-  internal.packageRecipes.chatgpt-desktop = ./_recipes/chatgpt-desktop.nix;
-
-  perSystem =
-    { pkgs, ... }:
-    {
-      packages.chatgpt-desktop = pkgs.chatgpt-desktop;
-    };
-}
