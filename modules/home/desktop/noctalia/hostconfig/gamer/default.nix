a9c635ba7a5d73ebd4fd8c9fe7e8e26160a7f307
Stian R. Sporaland <mail@stian.rs>
2026-09-05 20:37:43 +0200

    gamer vrr settings

--- modules/home/desktop/noctalia/hostconfig/gamer/default.nix
+++ modules/home/desktop/noctalia/hostconfig/gamer/default.nix
@@ -18,7 +18,7 @@
 
         wayland.windowManager.hyprland.extraConfig = ''
           hl.monitor({ output = "DP-2", mode = "highrr", position = "0x0", scale = 1 })
-          hl.monitor({ output = "DP-1", mode = "highrr", position = "0x1440", scale = 1, vrr = 1 })
+          hl.monitor({ output = "DP-1", mode = "highrr", position = "0x1440", scale = 1, vrr = 3})
           hl.monitor({ output = "DP-3", mode = "highrr", position = "2560x1189", scale = 1, transform = 3 })
           hl.monitor({ output = "", mode = "preferred", position = "auto", scale = 1 })
         '';
