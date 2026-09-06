1ba3bfbf5dbbc390b5ce477754a7bb6fd91927c5
Stian R. Sporaland <mail@stian.rs>
2026-09-06 17:11:33 +0200

    Fix laptop workspace rules

--- modules/home/desktop/noctalia/hostconfig/laptop/rules.nix
+++ modules/home/desktop/noctalia/hostconfig/laptop/rules.nix
@@ -12,15 +12,8 @@
       config = lib.mkIf cfg.enable {
         wayland.windowManager.hyprland.extraConfig = ''
           hl.workspace_rule({
-            workspace = "1",
-            default_name = "Discord",
-            monitor = "eDP-1",
+            workspace = "name:Discord",
             persistent = true,
-            gaps_in = 0,
-            gaps_out = 0,
-            no_rounding = true,
-            no_border = true,
-            decorate = false,
           })
         '';
       };
